46a7e099d53d1eeadb212b96c65da895f49edd17
[Shehzad Mohammed] <60529920+Shahza727@users.noreply.github.com>
2022-10-18 00:16:17 -0400

    Updated Terraform Outputs

--- Terraform-Infrastructure/outputs.tf
+++ Terraform-Infrastructure/outputs.tf
@@ -1,18 +1,18 @@
 output "primaryaccesskey" {
   depends_on  = [module.rt_py_kubedeployment]
-  value       = module.storage_account_and_resource_group.PKM
+  value       = module.rt_py_kubedeployment.PKM
   sensitive   = true
   description = "This is the primary access key. On the first run through the script, this creates the txt file to store this information which is later used as a PowerShell variable to authenticate."
 }
 output "storageaccname" {
   depends_on  = [module.rt_py_kubedeployment]
-  value       = module.storage_account_and_resource_group.SAN
+  value       = module.rt_py_kubedeployment.SAN
   sensitive   = false
   description = "This is the storage account name. On the first run through the script, this creates the txt file to store this information to identify the globally unique storage account."
 }
 output "storagecontname" {
   depends_on  = [module.rt_py_kubedeployment]
-  value       = module.storage_account_and_resource_group.SCA
+  value       = module.rt_py_kubedeployment.SCA
   sensitive   = false
   description = "This is the storage container name. On the first run through the script, this creates the txt file to store this information so the script can store the state file in the correct location."
 }
