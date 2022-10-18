192d673059903b2cca1c1c5295c5ec8a74dda651
[Shehzad Mohammed] <60529920+Shahza727@users.noreply.github.com>
2022-10-17 21:02:58 -0400

    updated context in powershell ~ just monorepo thingz

--- Terraform-Infrastructure/run.ps1
+++ Terraform-Infrastructure/run.ps1
@@ -5,7 +5,7 @@ $stringappend = "rtpystorage"
 $paramforstr = $stringappend + $date
 #Takes Parameter1 for Storage Account Name
 
-(Get-Content ./variables.tf).Replace('replace-variable', $paramforstr) | Set-Content ./variables.tf #This sets the variable to the input given
+(Get-Content ./Terraform-Infrastructure/variables.tf).Replace('replace-variable', $paramforstr) | Set-Content ./Terraform-Infrastructure/variables.tf #This sets the variable to the input given
 
 terraform init
 terraform plan 
@@ -16,17 +16,17 @@ terraform output -raw storagecontname > SCA.txt
 terraform output -raw PAK > PAK.txt
 #Builds TF and outputs certain data to txt files
 
-$date = (Get-Content ./don.txt).ForEach({ '"{0}"' -f $_ })
+$date = (Get-Content ./Terraform-Infrastructure//don.txt).ForEach({ '"{0}"' -f $_ })
 
 # $SAS = (Get-Content ./SAStoken.txt).ForEach({ '"{0}"' -f $_ })
-$SAN = (Get-Content ./SAN.txt).ForEach({ '"{0}"' -f $_ })
-$SCA = (Get-Content ./SCA.txt).ForEach({ '"{0}"' -f $_ })
-$PAK = (Get-Content ./PAK.txt).ForEach({ '"{0}"' -f $_ })
+$SAN = (Get-Content ./Terraform-Infrastructure//SAN.txt).ForEach({ '"{0}"' -f $_ })
+$SCA = (Get-Content ./Terraform-Infrastructure//SCA.txt).ForEach({ '"{0}"' -f $_ })
+$PAK = (Get-Content ./Terraform-Infrastructure//PAK.txt).ForEach({ '"{0}"' -f $_ })
 $key = "terraform.tfstate"
 #Formats and initializes the variables used in the migration process from local to azurerm 
 
 
-(Get-Content ./versions.tf).Replace('"local"', '"azurerm"') | Set-Content ./versions.tf
+(Get-Content ./Terraform-Infrastructure/versions.tf).Replace('"local"', '"azurerm"') | Set-Content ./Terraform-Infrastructure/versions.tf
 #Changes the script backend to azurerm 
 terraform init -migrate-state -force-copy `
     -backend-config="storage_account_name=$SAN" `
@@ -39,8 +39,8 @@ terraform plan
 terraform apply --auto-approve
 #Finishes the process with the new backend in azure storage account...
 Remove-Item SAN.txt, SCA.txt, SAStoken.txt, PAK.txt, terraform.tfstate, .\.terraform.lock.hcl, .terraform -Recurse -Force -Confirm:$false
-(Get-Content ./variables.tf).Replace($storageName, 'replace-variable') | Set-Content ./variables.tf
-(Get-Content ./versions.tf).Replace('"azurerm"', '"local"') | Set-Content ./versions.tf
+(Get-Content ./Terraform-Infrastructure/variables.tf).Replace($storageName, 'replace-variable') | Set-Content ./Terraform-Infrastructure/variables.tf
+(Get-Content ./Terraform-Infrastructure/versions.tf).Replace('"azurerm"', '"local"') | Set-Content ./Terraform-Infrastructure/versions.tf
 #This removes the unnecessary files from the local environment
 #Two changes over the lifecyle of this script
 ### NOTE : we can still can use the variables defined in powershell if need be. 
