5f3f44e4343843610b38e9386ac18504b91334de
[Shehzad Mohammed] <60529920+Shahza727@users.noreply.github.com>
2022-10-20 02:09:40 -0400

    updated workflow files

--- Terraform-Infrastructure/run.ps1
+++ Terraform-Infrastructure/run.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = "STOP"
 $env:TF_LOG="INFO"
-Set-Location ./Terraform-Infrastructure
+Set-Location Terraform-Infrastructure
 Get-Date -Format "HHmmMMddyyyy" > dateandtime.txt
 $date = (Get-Content ./dateandtime.txt)
 $stringappend = "rtpystorage"
@@ -18,13 +18,25 @@ terraform output -raw primaryaccesskey > primaryaccessskey.txt
 #Builds TF and outputs certain data to txt files
 
 # $SAS = (Get-Content ./SAStoken.txt).ForEach({ '"{0}"' -f $_ })
-$env:SAN = (Get-Content ./storageaccname.txt).ForEach({ '"{0}"' -f $_ })
-$env:SCA = (Get-Content ./storagecontname.txt).ForEach({ '"{0}"' -f $_ })
-$env:PAK = (Get-Content ./primaryaccessskey.txt).ForEach({ '"{0}"' -f $_ })
-$env:KEY = "terraform.tfstate"
+$SAN = (Get-Content ./storageaccname.txt).ForEach({ '"{0}"' -f $_ })
+$SCA = (Get-Content ./storagecontname.txt).ForEach({ '"{0}"' -f $_ })
+$PAK = (Get-Content ./primaryaccessskey.txt).ForEach({ '"{0}"' -f $_ })
+$KEY = "terraform.tfstate"
 #Formats and initializes the variables used in the migration process from local to azurerm 
 
+$ARM_ACCESS_KEY=$(az storage account keys list --resource-group rt-intra --account-name "$SAN" --query '[0].value' -o tsv).
+# "resource_group_name=rt-infra" 
 
+terraform init -migrate-state `
+-backend-config="storage_account_name="$SAN"" `
+-backend-config="access_key="$ARM_ACCESS_KEY"" `
+-backend-config="container_name="$SCA"" `
+-backend-config="key=terraform.tfstate"
+
+terraform plan
+terraform apply --auto-approve
+
+Remove-Item storagecontname.txt, primaryaccessskey.txt, terraform.tfstate, .\.terraform.lock.hcl, .terraform -Recurse -Force -Confirm:$false
 (Get-Content ./versions.tf).Replace('"local"', '"azurerm"') | Set-Content ./versions.tf
 #Changes the script backend to azurerm 
 
